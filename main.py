5c9d66ee96396d6760aeb81a6cf0d3a724a45b25
brealinxx <brealinfee@gmail.com>
2024-03-27 17:18:30 +0800

    feat: getNameByID

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from os import environ
 from datetime import datetime
 from dotenv import load_dotenv
 from lark_oapi.api.task.v2 import *
+from lark_oapi.api.contact.v3 import *
 
 load_dotenv()
 user_Access_Token = environ.get("USER_ACCESS_TOKEN")
@@ -16,25 +17,19 @@ def process_task_data(task_data):
     """
     processed_data = {
         '任务项': task_data['summary'],
-        '创建人': task_data['creator']['id'],
+        '创建人': GetNameByUserID(task_data['creator']['id']),
         '任务创建时间': TimeChange(task_data['created_at'])
     }
 
     return processed_data
 
 def main():
-    try:
-        response = init()
-    except:
-        if not init.success():
-            lark.logger.error(
-                f"client.task.v2.task.get failed, code: {response.code}, msg: {response.msg}, log_id: {response.get_log_id()}")
-            return
+    listOfTasks_response = GetListOfTasksRequest()
 
     # 处理业务结果
     #lark.logger.info(lark.JSON.marshal(response.data, indent=4))
 
-    json_str = lark.JSON.marshal(response.data, indent=4)
+    json_str = lark.JSON.marshal(listOfTasks_response.data, indent=4)
     data = json.loads(json_str)
 
     processed_data = process_task_data(data['task'])
@@ -60,7 +55,10 @@ def init():
         .enable_set_token(True) \
         .log_level(lark.LogLevel.DEBUG) \
         .build()
+    return client
 
+def GetListOfTasksRequest():
+    client  = init()
     request: GetTaskRequest = GetTaskRequest.builder() \
         .task_guid(task_Guid) \
         .user_id_type("open_id") \
@@ -68,9 +66,38 @@ def init():
 
     option = lark.RequestOption.builder().user_access_token(user_Access_Token).build()
     response: GetTaskResponse = client.task.v2.task.get(request, option)
+    if not response.success():
+            lark.logger.error(
+                f"client.task.v2.task.get failed, code: {response.code}, msg: {response.msg}, log_id: {response.get_log_id()}")
+            return
 
     return response
 
+def GetUserNameRequest(userID):
+    client  = init()
+    request: GetUserRequest = GetUserRequest.builder() \
+        .user_id(userID) \
+        .user_id_type("open_id") \
+        .department_id_type("open_department_id") \
+        .build()
+    
+    option = lark.RequestOption.builder().user_access_token(user_Access_Token).build()
+    response: GetUserResponse = client.contact.v3.user.get(request, option)
+    if not response.success():
+        lark.logger.error(
+            f"client.contact.v3.user.get failed, code: {response.code}, msg: {response.msg}, log_id: {response.get_log_id()}")
+        return
+    
+    return response
+
+def GetNameByUserID(userID):
+    response_data = json.loads(lark.JSON.marshal(GetUserNameRequest(userID).data, indent=4))
+    try:
+        name = response_data['user']['name']
+        return name
+    except KeyError as e:
+        print(f"KeyError: {e}")
+        return None
 
 def TimeChange(unixTime):
     timestamp = int(unixTime) / 1000
